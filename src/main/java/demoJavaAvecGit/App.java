eefa6c00e242eb967f28bf0405a74505434b6789
samuelstefancic <SAMUELSTEFANCIC@GMAIL.com>
2022-12-21 15:53:11 +0100

    changements majeurs structure

---
+++ src/main/java/demoJavaAvecGit/App.java
@@ -0,0 +1,13 @@
+package demoJavaAvecGit;
+
+public class App {
+
+	public static void main(String[] args) {
+		//c'est chouette le java
+		System.out.println("hello world");
+
+		System.out.println("youpi");
+
+	}
+
+}
